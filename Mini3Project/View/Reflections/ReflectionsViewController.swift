dd79827853554a400259281c2f8f5eeb7fb55272
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2024-08-18 08:13:58 +0700

    feat: implement SwiftData

--- Mini3Project/View/Reflections/ReflectionsViewController.swift
+++ Mini3Project/View/Reflections/ReflectionsViewController.swift
@@ -1,5 +1,10 @@
 import UIKit
 import Combine
+import SwiftData
+
+enum Section {
+    case reflections
+}
 
 class ReflectionsViewController: UIViewController {
     // MARK: - UI Elements
@@ -9,13 +14,72 @@ class ReflectionsViewController: UIViewController {
     var viewModel: ReflectionsViewModel = ReflectionsViewModel()
     private var cancellables: Set<AnyCancellable> = []
     
+    var container: ModelContainer?
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Reflection>?
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        container = try? ModelContainer(for: Reflection.self)
+        createCollectionView()
+        createDataSource()
+        loadReflections()
+        
+        generateData()
+        
         configureView()
         configureReflectionButton()
         configureDateLabel()
         bindViewModel()
+    }
+    
+    func generateData() {
+        let reflection1 = Reflection(topic: "topic 1", answers: ["ans1", "ans2"], createdAt: Date())
+        let reflection2 = Reflection(topic: "topic 2", answers: ["ans1", "ans2"], createdAt: Date())
+        let reflection3 = Reflection(topic: "topic 3", answers: ["ans1", "ans2"], createdAt: Date())
+
+        
+        container?.mainContext.insert(reflection1)
+        container?.mainContext.insert(reflection2)
+        container?.mainContext.insert(reflection3)
+        
+        loadReflections()
+    }
+    
+    func createCollectionView() {
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+        
+        collectionView.register(UICollectionViewListCell.self, forCellWithReuseIdentifier: "Reflection")
+    }
+    
+    func createDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Reflection>(collectionView: collectionView) { collectionView, indexPath, reflection in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Reflection", for: indexPath)
+            
+            var content = UIListContentConfiguration.cell()
+            content.text = "Relfection topic: \(reflection.topic)."
+            cell.contentConfiguration = content
+            
+            return cell
+        }
+    }
+    
+    func loadReflections() {
+        let descriptor = FetchDescriptor<Reflection>()
+        let reflections = (try? container?.mainContext.fetch(descriptor)) ?? []
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Reflection>()
+        snapshot.appendSections([.reflections])
+        snapshot.appendItems(reflections)
+        dataSource?.apply(snapshot, animatingDifferences: false)
     }
     
     // MARK: - View Configuration
@@ -40,13 +104,16 @@ class ReflectionsViewController: UIViewController {
             reflectionButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
             reflectionButton.widthAnchor.constraint(equalToConstant: 200),
             reflectionButton.heightAnchor.constraint(equalToConstant: 50)
-        ])    }
+        ])
+    }
     
     private func configureDateLabel() {
         dateLabel.text = getCurrentDateFormatted()
+        dateLabel.layer.cornerRadius = 8
+        dateLabel.backgroundColor = .systemGray5
         dateLabel.textColor = .black
         dateLabel.textAlignment = .center
-        dateLabel.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        dateLabel.font = UIFont.systemFont(ofSize: 17, weight: .medium)
         dateLabel.isUserInteractionEnabled = true
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(openDatePicker))
@@ -58,9 +125,9 @@ class ReflectionsViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             dateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            dateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            dateLabel.bottomAnchor.constraint(equalTo: reflectionButton.topAnchor, constant: -20)
+            dateLabel.widthAnchor.constraint(equalToConstant: 240),
+            dateLabel.heightAnchor.constraint(equalToConstant: 48),
+            dateLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32)
         ])
     }
     
@@ -102,6 +169,8 @@ class ReflectionsViewController: UIViewController {
             }
             .store(in: &cancellables)
     }
+    
+    
 }
 
 
