08c13d37b04ff9d6be44579f7fe6a08fd026c91b
raniapryanka <120316633+raniapryanka@users.noreply.github.com>
2024-08-21 15:37:58 +0700

    feat: add bridge chat view

--- Mini3Project/View/BridgeChatViewController.swift
+++ Mini3Project/View/BridgeChatViewController.swift
@@ -8,65 +8,281 @@
 import UIKit
 
 class BridgeChatViewController: UIViewController {
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var rectangleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "4stages")
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private lazy var descTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "You will go through 4 stages to recognize your strengths!"
+        label.textColor = .black
+        label.numberOfLines = 0
+
+        let customParagraphStyle = NSMutableParagraphStyle()
+        customParagraphStyle.alignment = .center
+        
+        var typographyAttributes = TypographyEmphasized.body
+        typographyAttributes[.paragraphStyle] = customParagraphStyle
+
+        let attributedText = NSAttributedString(
+            string: label.text ?? "",
+            attributes: typographyAttributes
+        )
+        
+        label.attributedText = attributedText
+        label.translatesAutoresizingMaskIntoConstraints = false
+    
+        
+        return label
+    }()
+    
+    
+    private lazy var descSubLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Let's make sure to do it correctly!"
+        label.textColor = .black
+        label.numberOfLines = 0
+
+        
+        let customParagraphStyle = NSMutableParagraphStyle()
+        customParagraphStyle.alignment = .center
+        
+        var typographyAttributes = TypographyRegular.caption1
+        typographyAttributes[.paragraphStyle] = customParagraphStyle
+
+        let attributedText = NSAttributedString(
+            string: label.text ?? "",
+            attributes: typographyAttributes
+        )
+        
+        label.attributedText = attributedText
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var durationView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "duration")
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    
+    private lazy var descTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10 min"
+        label.textColor = .black
+        label.numberOfLines = 0
+
+        
+        let customParagraphStyle = NSMutableParagraphStyle()
+        customParagraphStyle.alignment = .center
+        
+        var typographyAttributes = TypographyRegular.caption2
+        typographyAttributes[.paragraphStyle] = customParagraphStyle
+
+        let attributedText = NSAttributedString(
+            string: label.text ?? "",
+            attributes: typographyAttributes
+        )
+        
+        label.attributedText = attributedText
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    
+    private lazy var startButton: UIButton = {
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor(named: "BTint100")
+            button.layer.cornerRadius = 10
+            
+            let titleColor = UIColor(named: "BGLight")
+            
+            let attributedText = NSAttributedString(
+                string: "Start exploration 􀄫",
+                attributes: TypographyEmphasized.body.merging([
+                    .foregroundColor: titleColor!
+                ]) { (current, _) in current }
+            )
+            button.setAttributedTitle(attributedText, for: .normal)
+            
+            button.addTarget(self, action: #selector(startExplorationButtonTapped), for: .touchUpInside)
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            
+            return button
+        }()
+    
+  
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Create a rectangle view
-              let rectangleView = UIView()
-              
-              // Set the frame (position and size)
-              rectangleView.frame = CGRect(x: 70, y: 173, width: 252, height: 391)
-              
-              // Set the background color for the rectangle
-              rectangleView.backgroundColor = UIColor.lightGray
-              
-              // Add the rectangle to the main view
-              self.view.addSubview(rectangleView)
-              
-              // Create the first label
-              let label1 = UILabel()
-              label1.text = "Ready? Let's start reflecting!"
-              label1.font = UIFont.boldSystemFont(ofSize: 24)
-              label1.textAlignment = .center
-              label1.textColor = UIColor.white
-              label1.sizeToFit()
-              
-              // Position the first label below the rectangle with 24 points distance
-              label1.frame.origin = CGPoint(x: (self.view.frame.width - label1.frame.width) / 2,
-                                            y: rectangleView.frame.maxY + 24)
-              
-              self.view.addSubview(label1)
-              
-              // Create the second label
-              let label2 = UILabel()
-              label2.text = "Try to respond seriously and attentively to discover and understand your strengths!"
-              label2.font = UIFont.systemFont(ofSize: 18) // Regular font with size 18
-              label2.textAlignment = .center
-              label2.textColor = UIColor.white
-              label2.numberOfLines = 0 // Allow multiple lines
-              label2.frame = CGRect(x: 70, y: label1.frame.maxY + 8, width: 252, height: 0) // Initial height 0
-              
-              // Adjust the label size to fit the text
-              label2.sizeToFit()
-              
-              // Center the label horizontally and ensure it doesn't exceed the width of the rectangle
-              label2.frame.origin.x = 70 + (252 - label2.frame.width) / 2
-              
-              // Add the second label to the main view
-              self.view.addSubview(label2)
-        // Do any additional setup after loading the view.
+        
+        setupBrdigeChatView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func startExplorationButtonTapped() {
+        let alert = UIAlertController(title: "Middle Button", message: "Middle button tapped!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
-    */
 
+}
+
+extension BridgeChatViewController {
+    private func setupBrdigeChatView() {
+        self.view.backgroundColor = .systemBackground
+        
+        self.view.addSubview(contentView)
+//        contentView.backgroundColor = .red
+        
+        NSLayoutConstraint.activate([
+            contentView.heightAnchor.constraint(equalToConstant: 562),
+            
+            contentView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 54),
+            contentView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 70),
+            contentView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -70),
+        ])
+        
+//        
+//        contentView.addSubview(rectangleView)
+//        NSLayoutConstraint.activate([
+//            rectangleView.heightAnchor.constraint(equalToConstant: 391),
+//            
+//            rectangleView.topAnchor.constraint(equalTo: contentView.topAnchor),
+//            rectangleView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+//            rectangleView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+//        ])
+        
+        contentView.addSubview(imageView)
+//        imageView.backgroundColor = .systemPink
+        
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalToConstant: 282),
+            
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+           ])
+        
+        
+        
+        
+        contentView.addSubview(descTitleLabel)
+//        descTitleLabel.backgroundColor = .cyan
+        NSLayoutConstraint.activate([
+            descTitleLabel.heightAnchor.constraint(equalToConstant: 48),
+            
+            descTitleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16),
+            descTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            descTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+        ])
+        
+        
+        contentView.addSubview(descSubLabel)
+//        descSubLabel.backgroundColor = .blue
+        NSLayoutConstraint.activate([
+            descSubLabel.heightAnchor.constraint(equalToConstant: 16),
+            
+            descSubLabel.topAnchor.constraint(equalTo: descTitleLabel.bottomAnchor, constant: 8),
+            descSubLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            descSubLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+        ])
+        
+        
+        
+        contentView.addSubview(durationView)
+//        durationView.backgroundColor = .systemPink
+        
+        NSLayoutConstraint.activate([
+               durationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+               durationView.topAnchor.constraint(equalTo: descSubLabel.bottomAnchor, constant: 16),
+               durationView.widthAnchor.constraint(equalToConstant: 50),
+               durationView.heightAnchor.constraint(equalToConstant: 50)
+           ])
+        
+        contentView.addSubview(descTimeLabel)
+//        descTimeLabel.backgroundColor = .blue
+        NSLayoutConstraint.activate([
+            descTimeLabel.heightAnchor.constraint(equalToConstant: 16),
+            
+            descTimeLabel.topAnchor.constraint(equalTo: durationView.bottomAnchor, constant: 8),
+            descTimeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            descTimeLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+        ])
+        
+        
+        contentView.addSubview(startButton)
+//        startButton.backgroundColor = .blue
+        NSLayoutConstraint.activate([
+            startButton.heightAnchor.constraint(equalToConstant: 46),
+            
+            startButton.topAnchor.constraint(equalTo: descTimeLabel.bottomAnchor, constant: 164),
+            startButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            startButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+        ])
+        
+        
+        
+        
+        
+    }
+}
+
+// PREVIEW
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+        //
+    }
+    
+    let viewController: ViewController
+
+    init(_ builder: @escaping () -> ViewController) {
+        viewController = builder()
+    }
+
+    // MARK: - UIViewControllerRepresentable
+    func makeUIViewController(context: Context) -> ViewController {
+        viewController
+    }
+}
+#endif
+
+struct BestInClassPreviews_Previews: PreviewProvider {
+    static var previews: some View {
+        UIViewControllerPreview {
+            // Return whatever controller you want to preview
+            let vc = BridgeChatViewController()
+            return vc
+        }
+    }
 }
